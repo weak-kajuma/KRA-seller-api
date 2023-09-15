f90ae0b82940612e9e8af0b83c9b4701c0be0811
weak-kajuma <kazuma826826@gmail.com>
2023-09-15 21:12:19 +0900

    formationに対応

--- api/types/Ticket.ts
+++ api/types/Ticket.ts
@@ -8,3 +8,16 @@ export interface Ticket {
     bet: number
     race: number
 }
+
+export interface FormationTicket {
+    ticket_id?: number
+    user_id: string
+    f: number[]
+    s: number[]
+    t: number[]
+    type: string
+    option: string //常に"NO"
+    optNum: number
+    bet: number
+    race: number
+}
