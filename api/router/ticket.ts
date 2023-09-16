0a783055d0aa0cc8e85f3b69905552d132005a44
weak-kajuma <kazuma826826@gmail.com>
2023-09-16 13:12:25 +0900

    Fix wheel bug

--- api/router/ticket.ts
+++ api/router/ticket.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import { ResultSetHeader, RowDataPacket } from "mysql2";
 import mysql_connect from "./../utils/mysqlConnect";
 import { FormationTicket, Ticket } from "../types/Ticket";
-import { KRAFormation, TicketType } from "../kra/KRAClient";
+import { KRAFormation, KRATicket, TicketType } from "../kra/KRAClient";
 
 const router = express.Router();
 const con = mysql_connect();
@@ -45,7 +45,9 @@ router.get("/get/:ticket_id", async (req: Request, res: Response<Ticket>) => {
 router.post("/add", async (req: Request<Ticket>, res: Response<Ticket>) => {
     const [checkMoneyRows] = await con.query<RowDataPacket[]>("SELECT having_money FROM users WHERE user_id = ?", [req.body.user_id])
     if (checkMoneyRows.length === 0) throw new Error("The User is not found.")
-    const setMoney = checkMoneyRows[0].having_money - parseInt(req.body.bet)
+    const oisiiyammuy: KRATicket = new KRATicket(req.body.type, req.body.bet, req.body.horse, req.body.option, req.body.optNum, req.body.user_id)
+    const size = oisiiyammuy.build().length;
+    const setMoney = checkMoneyRows[0].having_money - parseInt(req.body.bet) * size;
     if (setMoney < 0) throw new Error("You do not have enough money to complete the bet.")
     const [addTicketRows] = await con.query<ResultSetHeader>(
         "INSERT INTO ticket (user_id, horse, type, option, optNum, bet, race) VALUES (?, JSON_ARRAY(?), ?, ?, ?, ?, ?)",
